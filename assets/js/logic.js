07491ff3e39b1e4a6eeeb7a7e1d26bd4bcb6da6e
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-01-24 16:12:50 +0000

    completed socre.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,50 +3,80 @@ let startPage = document.getElementById("start-screen")
 let questionPage = document.getElementById("questions")
 let endPage = document. getElementById("end-screen")
 let startButton = document.getElementById("start")
+let timerElement = document.getElementById("time");
+let time = questions.length * 15;
+let timeID;
 
 
 //click start button to start
 
-startButton.addEventListener("click", startGame())
+startButton.addEventListener("click", startQuiz)
 
-function startGame() {
-
-    startPage.classList.remove("start");
-    startPage.classList.add("hide");
-    questionPage.classList.remove("hide");
-    questionPage.classList.add("start");
-    
-
-    endQuiz = false;
-    timerCount = 1000;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
+function startQuiz() {
+    startPage.setAttribute("class", "hide");
+    questionPage.removeAttribute("class");    
+    time = setInterval(startTimer, 1000);
     startTimer()
-  }
+    getQuestion()
+} 
   
 // The setTimer function starts and stops the timer and triggers endQuiz()
-let timerElement = document.getElementById("time")
 
 function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
+//     Sets timer
+  time--;
+  timerElement.textContent = time;
 
-        if (endQuiz && timerCount > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          endQuiz();
-        }
-      }
-      // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        endQuiz();
-      }
-    }, 1000);
+  // if (time<=0){
+  //   endQuiz();
+  // }
+}
+//       if (timeID >= 0) {
+
+//         if (endQuiz && timeID > 0) {
+//           // Clears interval and stops timer
+//           clearInterval(timer);
+//           endQuiz();
+//         }
+//       }
+//       // Tests if time has run out
+//       if (timeID === 0) {
+//         // Clears interval
+//         clearInterval(timer);
+//         endQuiz();
+//       }
+//     }, 1000);
+//   }
+
+let titleElement = document.getElementById("question-title")
+let choicesElement = document.getElementById("choices")
+let currentQuestionIndex = 0;
+
+
+  function getQuestion () {
+  
+    let currentQuestion = questions[currentQuestionIndex];
+
+    titleElement.textContent = currentQuestion.title;
+
+    choicesElement.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choices, index){
+      let choicesButton = document.createElement("button");
+      choicesButton.setAttribute("class", "choice");
+      choicesButton.setAttribute("value", choices);
+      
+      choicesButton.textContent = `${choices}`
+
+      choicesButton.addEventListener("click", selectAnswer)
+
+      choicesElement.append(choicesButton);
+    })
+  }
+
+
+  function selectAnswer (){
+
   }
 
   // The endQuiz function is called when the end condition is met
@@ -55,10 +85,13 @@ let initial = document.getElementById("initials")
 let finalScore = document.getElementById("final-score")
 
   function endQuiz() {
+    clearInterval(timeID);
     questionPage.classList.remove("start");
     questionPage.classList.add("hide");
     endPage.classList.remove("hide");
     endPage.classList.add("start");
+
+    finalScore.textContent = time;
 
 
 
